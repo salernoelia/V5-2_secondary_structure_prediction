c1919781e60c428368e80d59667668e475e879fe
Elia Salerno <hello@eliasalerno.ch>
2025-12-02 16:02:14 +0100

    task 2

---
+++ viterbi.py
@@ -0,0 +1,41 @@
+import numpy as np
+from numba import jit
+
+# algorithm copied from https://www.audiolabs-erlangen.de/resources/MIR/FMP/C5/C5S3_Viterbi.html
+@jit(nopython=True)
+def viterbi(A, C, B, O):
+    """Viterbi algorithm for solving the uncovering problem
+
+    Args:
+        A (np.ndarray): State transition probability matrix of dimension I x I
+        C (np.ndarray): Initial state distribution  of dimension I
+        B (np.ndarray): Output probability matrix of dimension I x K
+        O (np.ndarray): Observation sequence of length N
+
+    Returns:
+        S_opt (np.ndarray): Optimal state sequence of length N
+        D (np.ndarray): Accumulated probability matrix
+        E (np.ndarray): Backtracking matrix
+    """
+    I = A.shape[0]    # Number of states
+    N = len(O)  # Length of observation sequence
+
+    # Initialize D and E matrices
+    D = np.zeros((I, N))
+    E = np.zeros((I, N-1)).astype(np.int32)
+    D[:, 0] = np.multiply(C, B[:, O[0]])
+
+    # Compute D and E in a nested loop
+    for n in range(1, N):
+        for i in range(I):
+            temp_product = np.multiply(A[:, i], D[:, n-1])
+            D[i, n] = np.max(temp_product) * B[i, O[n]]
+            E[i, n-1] = np.argmax(temp_product)
+
+    # Backtracking
+    S_opt = np.zeros(N).astype(np.int32)
+    S_opt[-1] = np.argmax(D[:, -1])
+    for n in range(N-2, -1, -1):
+        S_opt[n] = E[int(S_opt[n+1]), n]
+
+    return S_opt, D, E
